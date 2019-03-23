f2c5fe7421dbfafe0a9eda6962c331177b633111
Ashu <ashujha.1996@gmail.com>
2019-03-24 02:13:46 +0530

    no status bar in auth activity

--- app/src/main/java/com/pixerapps/placie/ui/activity/authentication/AuthActivity.kt
+++ app/src/main/java/com/pixerapps/placie/ui/activity/authentication/AuthActivity.kt
@@ -2,17 +2,25 @@ package com.pixerapps.placie.ui.activity.authentication
 
 import android.os.Bundle
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import com.pixerapps.placie.R
 import com.pixerapps.placie.mvp.BaseMvpActivity
 import com.pixerapps.placie.ui.activity.authentication.fragments.common.CommonFragment
 import com.pixerapps.placie.utils.Config
 
-class AuthActivity : BaseMvpActivity<AuthActivityContract.View, AuthActivityPresenter>(), AuthActivityContract.View{
+class AuthActivity : BaseMvpActivity<AuthActivityContract.View, AuthActivityPresenter>(), AuthActivityContract.View {
 
     override var presenter: AuthActivityPresenter = AuthActivityPresenter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
         setContentView(R.layout.activity_splash)
 
         showCommonFragment()
