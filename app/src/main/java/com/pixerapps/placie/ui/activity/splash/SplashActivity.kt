85ab203532f8b15e0b430fb0f82aae020ee72761
Ashu <ashujha.1996@gmail.com>
2019-03-26 01:06:30 +0530

    bottom nav in home activity

--- app/src/main/java/com/pixerapps/placie/ui/activity/splash/SplashActivity.kt
+++ app/src/main/java/com/pixerapps/placie/ui/activity/splash/SplashActivity.kt
@@ -1,6 +1,8 @@
 package com.pixerapps.placie.ui.activity.splash
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import com.pixerapps.placie.R
 import com.pixerapps.placie.mvp.BaseMvpActivity
 
@@ -10,6 +12,9 @@ class SplashActivity : BaseMvpActivity<SplashContract.View, SplashPresenter>(),S
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        window.statusBarColor = Color.WHITE
+
         presenter.moveToNextActivity(this)
     }
 }
--- app/src/main/java/com/pixerapps/placie/ui/activity/user/home/HomeActivity.kt
+++ app/src/main/java/com/pixerapps/placie/ui/activity/user/home/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.pixerapps.placie.ui.activity.user.home
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import com.pixerapps.placie.R
 import com.pixerapps.placie.mvp.BaseMvpActivity
 
@@ -10,5 +12,7 @@ class HomeActivity : BaseMvpActivity<HomeActivityContract.View,HomeActivityPrese
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_home)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        window.statusBarColor = Color.WHITE
     }
 }
