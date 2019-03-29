7b57603314144f8800e56e203651b3906dc6f294
Ashu <ashujha.1996@gmail.com>
2019-03-30 00:02:41 +0530

    post model and adapter complete

---
+++ app/src/main/java/com/pixerapps/placie/model/PostPojo.kt
@@ -0,0 +1,41 @@
+package com.pixerapps.placie.model
+
+import com.google.gson.annotations.SerializedName
+
+data class PostPojo(
+    @SerializedName("message") val message: String,
+    @SerializedName("status") val success: Boolean,
+    @SerializedName("data") val data: List<PostData>
+)
+
+data class PostData(
+    @SerializedName("__v") val v: String,
+    @SerializedName("_id") val id: String,
+    @SerializedName("post_id") val postId: String,
+    @SerializedName("post_body") val postBody: String,
+    @SerializedName("post_title") val postTitle: String,
+    @SerializedName("category") val category: String,
+    @SerializedName("language") val language: String,
+    @SerializedName("status") val status: String,
+    @SerializedName("country") val country: String,
+    @SerializedName("created_at") val createdAt: String,
+    @SerializedName("updated_at") val updatedAt: String,
+    @SerializedName("contributor_name") val contributorName: String,
+    @SerializedName("contributor_id") val contributorId: String,
+    @SerializedName("contributor_institute") val contributorInstitute: String,
+    @SerializedName("comments") val comments: List<Comments>,
+    @SerializedName("is_job_post") val isJobPost: String,
+    @SerializedName("external_url") val externalUrl: String,
+    @SerializedName("image") val image: String,
+    @SerializedName("video") val video : String
+)
+
+data class Comments(
+    @SerializedName("__v") val v: String,
+    @SerializedName("_id") val id: String,
+    @SerializedName("comment_id") val commentId: String,
+    @SerializedName("comment_body") val commentBody: String,
+    @SerializedName("commented_at") val commentedAt: String,
+    @SerializedName("commented_by") val commentedBy: String,
+    @SerializedName("image") val image: String
+)
---
+++ app/src/main/java/com/pixerapps/placie/ui/adapter/PostAdapter.kt
@@ -0,0 +1,84 @@
+package com.pixerapps.placie.ui.adapter
+
+import android.app.Activity
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.pixerapps.placie.R
+import com.pixerapps.placie.model.PostData
+
+class PostAdapter(var list: List<PostData>, var activity: Activity) : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): PostAdapter.ViewHolder {
+        return ViewHolder(LayoutInflater.from(p0.context).inflate(R.layout.layout_post, p0, false))
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(viewHolder: PostAdapter.ViewHolder, pos: Int) {
+        viewHolder.bindData(list[pos], pos)
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+        var currentPos: Int = 0
+
+
+        var user_dp: ImageView = itemView.findViewById(R.id.user_dp)
+        var user_name: TextView = itemView.findViewById(R.id.user_name)
+        var institution: TextView = itemView.findViewById(R.id.institution)
+        var posted_at: TextView = itemView.findViewById(R.id.posted_at)
+        var post_title: TextView = itemView.findViewById(R.id.post_title)
+        var post_body: TextView = itemView.findViewById(R.id.post_body)
+        var post_image: TextView = itemView.findViewById(R.id.post_image)
+        var comment_count: TextView = itemView.findViewById(R.id.comment_count)
+        var apply_btn: TextView = itemView.findViewById(R.id.apply_btn)
+        var comment_box: TextView = itemView.findViewById(R.id.comment_box)
+
+        fun bindData(data: PostData, pos: Int) {
+            this.currentPos = pos
+
+            user_name.text = data.contributorName
+            institution.text = data.contributorInstitute
+            posted_at.text = data.createdAt
+            post_title.text = data.postTitle
+            post_body.text = data.postBody
+            comment_count.text = data.comments.size.toString() + " comments"
+            if (data.isJobPost.contentEquals("true")) {
+                apply_btn.visibility = View.VISIBLE
+                apply_btn.setOnClickListener(this)
+            }
+
+            comment_box.setOnClickListener(this)
+            comment_count.setOnClickListener(this)
+            apply_btn.setOnClickListener(this)
+            post_body.setOnClickListener(this)
+
+        }
+
+
+        override fun onClick(v: View?) {
+            when (v?.id) {
+                R.id.apply_btn -> {
+
+                }
+
+                R.id.comment_box -> {
+
+                }
+
+                R.id.post_body -> {
+
+                }
+
+                R.id.comment_count -> {
+
+                }
+            }
+        }
+    }
+}
